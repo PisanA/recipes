461e84e0a110e8709388339d14f039b51b2b69a3
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-05-17 15:48:27 -0700

    Update App.java

--- src/main/java/edu/uwb/App.java
+++ src/main/java/edu/uwb/App.java
@@ -2,7 +2,7 @@ package edu.uwb;
 
 /**
  * Hello world!
- * This is a simple introduction
+ * This is a simple introduction buidl in Java
  */
 public class App {
     public static void main( String[] args )
